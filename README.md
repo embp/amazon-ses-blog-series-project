372e7568fa4c79063b14a4e6711515ca29e1ee45
Daniel Montano <daniel@embusinessproducts.com>
2022-11-15 22:44:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example code from the Amazon SES transactional emailer series
+
+Project uses the [Simple Typescript Starter](https://github.com/stemmlerjs/simple-typescript-starter) project from [khalilstemmler.com](https://khalilstemmler.com).
---
+++ src/ses-service.ts
@@ -0,0 +1,51 @@
+import {
+  SESClient,
+  SendEmailCommand,
+  SendEmailCommandInput,
+  SendEmailCommandOutput,
+  Template,
+  CreateTemplateCommand,
+  SendTemplatedEmailCommandInput,
+  SendTemplatedEmailCommand,
+  DeleteTemplateCommand,
+} from '@aws-sdk/client-ses';
+
+export default class SesService {
+  public sesClient: SESClient;
+
+  constructor() {
+    this.sesClient = new SESClient({
+      region: 'us-east-1',
+      credentials: {
+        accessKeyId: 'AWS_API_ACCESS_KEY_ID',
+        secretAccessKey: 'AWS_API_SECRET_ACCESS_KEY',
+      },
+    });
+  }
+
+  public send(params: SendEmailCommandInput): Promise<SendEmailCommandOutput> {
+    return this.sesClient.send(new SendEmailCommand(params));
+  }
+
+  public createTemplate(template: Template) {
+    return this.sesClient.send(
+      new CreateTemplateCommand({
+        Template: template,
+      }),
+    );
+  }
+
+  public sendWithTemplate(
+    params: SendTemplatedEmailCommandInput,
+  ): Promise<SendEmailCommandOutput> {
+    return this.sesClient.send(new SendTemplatedEmailCommand(params));
+  }
+
+  public deleteTemplate(templateName: string) {
+    return this.sesClient.send(
+      new DeleteTemplateCommand({
+        TemplateName: templateName,
+      }),
+    );
+  }
+}
